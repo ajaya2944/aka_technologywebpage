9799f624b2b905d7c902183625b9fcdb3488460a
AJAYA KANDEL <a.k.a2023technology@gmail.com>
2025-02-11 15:39:29 +0900

    Ignored .env file to prevent sensitive data exposure

--- aka_technology/settings.py
+++ aka_technology/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os   # Add this line
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'website',
 ]
 
@@ -53,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this for serving static files in production
+    # Other middleware...
 ]
 
 ROOT_URLCONF = 'aka_technology.urls'
@@ -122,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # <-- Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -138,7 +142,8 @@ EMAIL_HOST_PASSWORD = 'nofm hvij nbve dsnq'  # Your Gmail password or an App Pas
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         BASE_DIR / "website/static",  # Add this line
-    BASE_DIR / "static",  # Keep this if you have global static files
+    BASE_DIR / "static", 
+    os.path.join(BASE_DIR, 'static') # Keep this if you have global static files
 ] # This is where Django will collect all static files for deployment
 
 # Media files (uploads)
